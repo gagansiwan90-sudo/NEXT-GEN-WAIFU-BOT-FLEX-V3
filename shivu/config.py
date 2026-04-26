aba592063151d5cc193b314662b29bb7bf6651c2
gagansiwan90-sudo <gagansiwan90@gmail.com>
2026-04-26 21:23:04 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,26 +1,31 @@
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "8546535996"
-    sudo_users = "8053803602"
+    OWNER_ID = 8546535996
+    SUDO_USERS = [8053803602]
+
     GROUP_ID = -1003942224732
-    TOKEN = "8768582373:AAHHznGAcIvtoKkYE3CJxhu6nHKRh3Kz5JQ"
-    mongo_url = "mongodb+srv://Elevenyts:Elevenyts@cluster0.vuyc1u2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    TOKEN = "YOUR_BOT_TOKEN"
+
+    API_ID = 34224204
+    API_HASH = "0dfa074770803265e8c61ddda0ad6fb0"
+
+    MONGO_URL = "YOUR_MONGO_URL"
+
+    PHOTO_URL = [
+        "https://telegra.ph/file/b925c3985f0f325e62e17.jpg",
+        "https://telegra.ph/file/4211fb191383d895dab9d.jpg"
+    ]
+
     SUPPORT_CHAT = "AdamBot_support"
     UPDATE_CHAT = "waifugrabbbbotupdates"
     BOT_USERNAME = "waifuhub_officialBot"
-    CHARA_CHANNEL_ID = "-1003985910295"
-api_id = "34224204"
-    api_hash = "0dfa074770803265e8c61ddda0ad6fb0"
-
-    
-
-    
-class Production(Config):
-    LOGGER = True
+    CHARA_CHANNEL_ID = -1003985910295
 
 
 class Development(Config):
+    LOGGER = True
+
+
+class Production(Config):
     LOGGER = True
